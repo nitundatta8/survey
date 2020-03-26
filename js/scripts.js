808daf3533a05f7905303c4d48d628c28e2fbffd
nitunwa <nitun.2@gmail.com>
2020-03-25 21:58:07 -0700

    add form input into a table

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,39 @@
+ 
+ var count = 1;
  $(document).ready(function(){
   $("#formOne").submit(function(event) {
-    event.preventDefault();
+    
+    console.log("value");
     var firstname = $("#firstName").val();
+    $("#firstName").val('');
     var lastname = $("#lastName").val();
-
-    $(".firstName").text(firstNameInput);
-    $(".lastName").text(lastNameInput)
-  });
-
-
-  $("#age").submit(function(event) {
-    event.preventDefault();
+    $("#lastName").val('');
     var age = $("#age").val();
-    $(".age").text(age); 
+    $("#age").val('');
+    var flavor = $("input:radio[name=flavor]:checked").val();
+    $("input:radio[name=flavor]:checked").val('');
+    var dateInput = $("#faveDate").val();
+    $("#faveDate").val('');
+    
+    
+  // table
+    
+
+  var row ='<tr>' +
+   '<th>'+ (count++) +'</th>' +
+    '<td>'+ firstname + '</td>' +
+    '<td>'+ lastname +'</td>'+
+    '<td>'+ age +'</td>' +
+    '<td>'+ flavor +'</td>' +
+    '<td>'+ dateInput +'</td>'+ 
+    '</tr>'
+
+
+   // $(".table").append(markup);
+    $(".table tbody").append(row);
+
+    event.preventDefault();
+//table
   });
- 
 
-
-  $("#output").show();
-
-  event.preventDefault();
 });
